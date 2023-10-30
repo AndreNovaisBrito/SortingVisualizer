ff0224d6fd183d31fa3806928fa09188480354c2
AndreNovaisBrito <andrenoz@gmail.com>
2023-10-30 15:30:17 -0300

    first commit

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class SortingVisualizer extends Component {
+    render() {
+      return (
+        <div>
+          <h1>Hello, I'm a React Component!</h1>
+          <p>This is a basic React component.</p>
+        </div>
+      );
+    }
+  }
