f915ec3de58a233b3f9d5a4e8b343090c23e4c29
AndreNovaisBrito <andrenoz@gmail.com>
2023-10-31 20:30:56 -0300

    InsertionSort Done

--- src/SortingVisualizer/SortingAlgorithms.js
+++ src/SortingVisualizer/SortingAlgorithms.js
@@ -1,16 +1,19 @@
 export function generateInsertionSortAnimations(array) {
-  let animations = [];
+  const animations = [];
+
   for (let i = 1; i < array.length; i++) {
     let a = i;
     while (a >= 1 && array[a] < array[a - 1]) {
       // Swap the elements
+      animations.push([a, a - 1]);
+      animations.push([a, a - 1]);
+
       let temp = array[a];
       array[a] = array[a - 1];
       array[a - 1] = temp;
 
       // Store the animations (indexes to swap)
       animations.push([a, a - 1]);
-
       a--;
     }
   }
